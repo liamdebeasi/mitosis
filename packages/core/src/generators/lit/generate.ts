2d24340066ed68cae34ce145de8599886da9c40a
Liam DeBeasi <liamdebeasi@users.noreply.github.com>
2024-06-05 15:42:06 -0400

    fix: migrate lit generator

--- packages/core/src/generators/lit/generate.ts
+++ packages/core/src/generators/lit/generate.ts
@@ -84,7 +84,13 @@ const blockToLit = (json: MitosisNode, options: ToLitOptions = {}): string => {
 
   for (const key in json.properties) {
     const value = json.properties[key];
-    str += ` ${key}="${value}" `;
+    if (!value) { continue; }
+    
+    if (value.type === 'string') {
+      str += ` ${key}="${value.code}" `;
+    } else {
+      str += ` .${key}=\${${value.code}} `;
+    }
   }
   for (const key in json.bindings) {
     const { code, arguments: cusArgs = ['event'], type } = json.bindings[key]!;
